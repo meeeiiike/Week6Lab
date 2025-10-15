c6bda75f782b79e9263c989b4b3e032490dba317
meike <g00407095@atu.ie>
2025-10-15 14:34:25 +0100

    Created model class. Included Lomkbok + Validation.

--- src/main/java/ie/atu/week5lab3/model/Passenger.java
+++ src/main/java/ie/atu/week5lab3/model/Passenger.java
@@ -1,4 +1,20 @@
 package ie.atu.week5lab3.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor@AllArgsConstructor@Data@Builder
 public class Passenger {
+    @NotBlank @Size(max=40)
+    private String passengerID;
+    @NotBlank @Size(max=60)
+    private String name;
+    @NotBlank @Email
+    private String email;
 }
