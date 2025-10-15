ccca1ddb6ae41e9cc37bada00b04ffd1ab94d5f9
meike <g00407095@atu.ie>
2025-10-15 14:49:06 +0100

    Created service class. Uses new Optional keyword which allows us to test true/false cases and return value regardless using optional's methods

--- src/main/java/ie/atu/week5lab3/service/PassengerService.java
+++ src/main/java/ie/atu/week5lab3/service/PassengerService.java
@@ -1,4 +1,38 @@
 package ie.atu.week5lab3.service;
 
+import ie.atu.week5lab3.model.Passenger;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PassengerService {
+    private final List<Passenger> store = new ArrayList<>();
+
+    // Defensive Copy of list to protect against changes made (prevent corruption/mistakes)
+    public List<Passenger> findAll(){
+        return new ArrayList<>(store);
+    }
+
+    // Uses Optional, which allows us to test true/false cases and return regardless using Optionals methods
+    public Optional<Passenger> findByID(String id){
+        for (Passenger p : store){
+            if(p.getPassengerID().equals(id)){
+                return Optional.of(p);
+            }
+        }
+        return Optional.empty();
+    }
+
+    // Uses findByID method to ensure no duped ID's are entered
+    public Passenger create(Passenger p){
+        if(findByID(p.getPassengerID()).isPresent()){
+            throw new IllegalArgumentException("Passenger already exists");
+        }
+        store.add(p);
+        return p;
+    }
+
 }
